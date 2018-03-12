61b0b431f234c8ae9cdeae9337eb9c4d48880f36
elen <1091591225@qq.com>
2018-03-13 00:21:33 +0800

    the second commit

--- ipPools/db.py
+++ ipPools/db.py
@@ -1,53 +1,64 @@
 import pymysql
 from getProxy import Proxy
 import requests
+from requests.adapters import HTTPAdapter
 import time
 import random
-#   定义待抓取页面
-xiCiUrl = ['http://www.xicidaili.com/nn/{}'.format(str(i)) for i in range(1, 16, 1)]
+xiCiUrl = ['http://www.xicidaili.com/nn/{}'.format(str(i)) for i in range(1, 55, 1)]
 wuYouUrl = ['http://www.data5u.com/free/gngn/index.shtml',
             'http://www.data5u.com/free/gnpt/index.shtml',
             'http://www.data5u.com/free/gwgn/index.shtml',
             'http://www.data5u.com/free/gwpt/index.shtml']
-url_66 = ['http://www.66ip.cn/{}.html'.format(str(i)) for i in range(1, 16, 1)]
-url_181 = ['http://www.ip181.com/daili/{}.html'.format(str(i)) for i in range(1, 16, 1)]
-kuaiUrl = ['https://www.kuaidaili.com/free/inha/{}/'.format(str(i)) for i in range(1, 16, 1)]
+url_66 = ['http://www.66ip.cn/{}.html'.format(str(i)) for i in range(1, 55, 1)]
+url_181 = ['http://www.ip181.com/daili/{}.html'.format(str(i)) for i in range(1, 55, 1)]
+kuaiUrl = ['https://www.kuaidaili.com/free/inha/{}/'.format(str(i)) for i in range(1, 55, 1)]
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
 }
 
 
 def xiCiInsert(url, header):
-    i = 1
     proxy = Proxy()
     proxies, types, dates = proxy.proxyXiCi(url, header)
-    time.sleep(5 + random.randint(2, 9))
+    #   设置最大请求次数
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=2)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     for single_proxy, single_type, date in zip(proxies, types, dates):
-        checkproxy = {'proxy': single_proxy}
+        check_proxy = {
+            'http': 'http://' + single_proxy,
+            'https': 'https://' + single_proxy
+        }
         try:
-            #   检测代理是否可用
-            requests.get(url='http://www.baidu.com/', headers=header, proxies=checkproxy, timeout=2)
+            #   验证代理是否可用
+            session.get(url='http://sz.58.com/chuzu/pn1/', headers=header, proxies=check_proxy, timeout=3)
         except:
             print('xici_connect_failed')
         else:
-            #   插入可用代理到对应表
             sql = ''' insert into proxy_xici(time, proxy, type)
                 values(%s, %s, %s );'''
             cur.execute(sql, (date, single_proxy, single_type))
             db.commit()
-            print('success_xici_%s' % i)
-            i += 1
+            print('success_xici')
+            
 
 
 def wuYouInsert(url, header):
-    i = 1
     proxy = Proxy()
     proxies, types = proxy.proxyWuYou(url, header)
-    time.sleep(5 + random.randint(2, 9))
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=2)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     for single_proxy, single_type in zip(proxies, types):
-        checkproxy = {'proxy': single_proxy}
+        check_proxy = {
+            'http': 'http://' + single_proxy,
+            'https': 'https://' + single_proxy
+        }
+        print(check_proxy)
         try:
-            requests.get(url='http://www.baidu.com/', headers=header, proxies=checkproxy, timeout=2)
+            session.get(url='http://sz.58.com/chuzu/pn1/', headers=header, proxies=check_proxy, timeout=3)
         except:
             print('wuyou_connect_failed')
         else:
@@ -55,19 +66,24 @@ def wuYouInsert(url, header):
             values(%s, %s);'''
             cur.execute(sql, (single_proxy, single_type))
             db.commit()
-            print('success_wuyou_%s' % i)
-            i += 1
+            print('success_wuyou')
+            
 
 
 def kuaiInsert(url, header):
     proxy = Proxy()
-    i = 1
     proxies, types, dates = proxy.proxyKuaiDaiLi(url, header)
-    time.sleep(5 + random.randint(2, 9))
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=2)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     for single_proxy, single_type, date in zip(proxies, types, dates):
-        checkproxy = {'proxy': single_proxy}
+        check_proxy = {
+            'http': 'http://' + single_proxy,
+            'https': 'https://' + single_proxy
+        }
         try:
-            requests.get(url='http://www.baidu.com/', headers=header, proxies=checkproxy, timeout=2)
+            session.get(url='http://sz.58.com/chuzu/pn1/', headers=header, proxies=check_proxy, timeout=3)
         except:
             print('kuai_connect_failed')
         else:
@@ -75,19 +91,24 @@ def kuaiInsert(url, header):
             values(%s, %s, %s );'''
             cur.execute(sql, (date, single_proxy, single_type))
             db.commit()
-            print('success_kuai_%s' % i)
-            i += 1
+            print('success_kuai')
+            
 
 
 def proxy66Insert(url, header):
     proxy = Proxy()
-    i = 1
     proxies, dates = proxy.proxy_66(url, header)
-    time.sleep(5 + random.randint(2, 9))
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=2)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     for single_proxy, date in zip(proxies, dates):
-        checkproxy = {'proxy': single_proxy}
+        check_proxy = {
+            'http': 'http://' + single_proxy,
+            'https': 'https://' + single_proxy
+        }
         try:
-            requests.get(url='http://www.baidu.com/', headers=header, proxies=checkproxy, timeout=2)
+            session.get(url='http://sz.58.com/chuzu/pn1/', headers=header, proxies=check_proxy, timeout=3)
         except:
             print('connect_66_failed')
         else:
@@ -95,19 +116,25 @@ def proxy66Insert(url, header):
             values(%s, %s);'''
             cur.execute(sql, (date, single_proxy))
             db.commit()
-            print('success_66_%s' % i)
-            i += 1
+            print('success_66')
+            
 
 
 def proxy181Insert(url, header):
     proxy = Proxy()
-    i = 1
     proxies, types, dates = proxy.proxy_181(url, header)
-    time.sleep(5 + random.randint(2, 9))
+	# 设置最大请求次数
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=2)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     for single_proxy, single_type, date in zip(proxies, types, dates):
-        checkproxy = {'proxy': single_proxy}
+        check_proxy = {
+            'http': 'http://' + single_proxy,
+            'https': 'https://' + single_proxy
+        }
         try:
-            requests.get(url='http://www.baidu.com/', headers=header, proxies=checkproxy, timeout=2)
+            session.get(url='http://sz.58.com/chuzu/pn1/', headers=header, proxies=check_proxy, timeout=3)
         except:
             print('connect_181_failed')
         else:
@@ -115,25 +142,34 @@ def proxy181Insert(url, header):
             values(%s, %s, %s );'''
             cur.execute(sql, (date, single_proxy, single_type))
             db.commit()
-            print('success_181_%s' % i)
-            i += 1
+            print('success_181')
+            
 
 
-def main(xici, wuyou, kuai, ip66, ip181):
-    for url in xici:
-        xiCiInsert(url, headers)
+def main(wuyou, xici, kuai, ip66, ip181):
     for url in wuyou:
+        print('=================================')
+        time.sleep(random.randint(2, 5))
         wuYouInsert(url, headers)
+    for url in xici:
+        time.sleep(random.randint(2, 5))
+        xiCiInsert(url, headers)
     for url in kuai:
+        time.sleep(random.randint(2, 5))
         kuaiInsert(url, headers)
     for url in ip66:
+        time.sleep(random.randint(2, 5))
         proxy66Insert(url, headers)
     for url in ip181:
+        time.sleep(random.randint(2, 5))
         proxy181Insert(url, headers)
 
 
 if __name__ == '__main__':
     db = pymysql.connect(host="localhost", user="root", password="123456", db="ippools", charset="utf8")
     cur = db.cursor()
-    main(xiCiUrl, wuYouUrl, kuaiUrl, url_66, url_181)
+    main(wuYouUrl, xiCiUrl, kuaiUrl, url_66, url_181)
     db.close()
+
+
+
--- ipPools/refresh.py
+++ ipPools/refresh.py
@@ -1,42 +1,62 @@
 import pymysql
 import requests
 from db import main
+from requests.adapters import HTTPAdapter
+from bs4 import BeautifulSoup
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
 }
-# 更新数据库
-
+#	更新数据库
 
 def checkProxy(table_name, header):
     db = pymysql.connect(host='localhost', user='root', password='123456', db='ippools', charset='utf8')
     cur = db.cursor()
-    cur.execute('select max(time) from %s' % table_name)
-    # 获得最近日期 便于更新时定义待抓取页面
-    latest_date = cur.fetchall()
-    print(latest_date)
-    cur.execute('select proxy from %s' % table_name)
+	# 获取最近日期
+    # cur.execute('select max(time) from %s' % table_name)
+    # latest_date = cur.fetchall()
+    # print(latest_date)
+    cur.execute('select proxy from %s ;' % table_name)
     proxies = cur.fetchall()
+    a = 1
+    b = 1
+    session_1 = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=5)
+    session_1.mount("http://", adapter)
+    session_1.mount("https://", adapter)
     for proxy in proxies:
-        #   遍历整张表 删除不可用的代理
-        print(proxy[0])
-        check_proxy = {'proxy': proxy[0]}
+        check_proxy = {
+            "http": 'http://' + proxy[0],
+            "https": 'https://' + proxy[0]
+        }
+        print(check_proxy)
         try:
-            requests.get(url='http://www.baidu.com/', headers=header, proxies=check_proxy, timeout=2)
+            data = session_1.get(url='https://httpbin.org/get?show_env=1', headers=header, proxies=check_proxy, timeout=2).text
+            print('可用%d' % a)
+            print(data)#查看代理是否启用
+            a += 1
         except:
             print('connect_%s_failed' % table_name)
-            cur.execute('delete from %s where proxy=%s' % (table_name, proxy[0]))
-        else:
-            pass
+            print('failed %d' % b)
+            b += 1
+            sql = 'delete from %s where proxy=\'%s\'; ' % (table_name, proxy[0])
+            #   查看语句格式是否正确
+            print(sql)
+            cur.execute(sql)
+            db.commit()
 
 
 def refreshProxy(url1, url2, url3, url4, url5):
-    #   传入待抓取页面 更新数据库
     main(xici=url1, wuyou=url2, kuai=url3, ip66=url4, ip181=url5)
 
 
 if __name__ == '__main__':
 
-    checkProxy()
-    refreshProxy()
+    checkProxy('proxy_wuyou', headers)
+    checkProxy('proxy_xici', headers)
+    checkProxy('proxy_66', headers)
+    checkProxy('proxy_181', headers)
+    checkProxy('proxy_kuai', headers)
+    checkProxy('proxy_wuyou', headers)
+    # refreshProxy()
 
 
